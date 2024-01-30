1e468efa0f718f3b94ac3b0610fc4ebfa0fc1315
wmoreno33 <127151377+wmoreno33@users.noreply.github.com>
2024-01-30 11:00:44 -0500

    configuracion del proyecto

---
+++ usuarios/usuarios/settings/local.py
@@ -0,0 +1,33 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": get_secret("DB_NAME"),
+        "USER": get_secret("USER"),
+        "PASSWORD": get_secret("PASSWORD"),
+        "HOST": "localhost",
+        "PORT": "5432",
+    }
+}
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR.child('static')]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.child('media')
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
